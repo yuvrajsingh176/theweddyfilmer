74434bedea8d16d709e166c39b7dd7997e5e9ef4
Satyam Sah <satnarayan5166@gmail.com>
2024-11-16 21:03:48 +0545

    app set

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
+
+import Home from "./Page/Home";
+import Photography from "./Page/Photography";
+import Film from "./Page/Film";
+import Faq from "./Page/Faq";
+import Contact from "./Page/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="photography" element={<Photography />}></Route>
+            <Route path="Films" element={<Film />}></Route>
+            <Route path="Faq" element={<Faq />}></Route>
+            <Route path="Contact" element={<Contact />}></Route>
+            
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
