13ddd29d9dd568a5a2f9217c8c6556b184f5cfd8
Yuvraj <ducatimonster729@gmail.com>
2024-11-18 12:12:38 +0545

    .

--- src/components/Home/HomeShowcaseFilm.jsx
+++ src/components/Home/HomeShowcaseFilm.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-
+import React from "react";
 
 const HomeShowcaseFilm = () => {
   return (
-    <div>
-         <div className="videoGallary relative flex items-center justify-center mt-16 mb-16">
+      <div className="videoGallary relative flex items-center justify-center mt-16 mb-16 w-full">
         {/* this is the video VideoGallaryv */}
 
-        <video id='weddingVideos' 
-        
-        src="https://cdn.pixabay.com/video/2016/09/14/5278-182817488_large.mp4"
-        autoPlay muted loop />
+        <video
+          id="weddingVideos"
+          src="https://cdn.pixabay.com/video/2016/09/14/5278-182817488_large.mp4"
+          autoPlay
+          muted
+          loop
+          className=""
+        />
         <div className=" absolute text-3xl font-myFont text-white">
           Showcashing Films
         </div>
       </div>
-    </div>
-  )
-}
+  );
+};
 
-export default HomeShowcaseFilm
+export default HomeShowcaseFilm;
